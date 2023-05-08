015e4801e116e17bb8be4aedc854628fb7493cc6
Haeseul <jjchsjj@gmail.com>
2023-05-09 03:05:53 +0900

    Word Loading...Done

--- src/Word.js
+++ src/Word.js
@@ -1,4 +1,4 @@
-import { WORD_LOAD_PERIOD, WORD_SIZE, SEA_LEVEL } from "./Constants";
+import { WORD_LOAD_PERIOD, WORD_SIZE, SEA_LEVEL, DROP_RATE } from "./Constants";
 import randomWords from 'random-words';
 
 const textInput = document.getElementById('textInput');
@@ -16,18 +16,32 @@ class Word {
         this.velocity = velocity;
     }
 
+    isVisible() {
+        return this.visible;
+    }
+
     draw() {
         if(this.visible) {
+            fill(0);
             textSize(WORD_SIZE);
+            textAlign(CENTER);
             text(this.content, this.x, this.y);
         }
+        // this.drop();
+    }
+
+    drop() {
+        this.y = this.y + DROP_RATE;
+    }
+
+    inOcean() {
+        if(this.y >= SEA_LEVEL) this.visible = false;
     }
 
     submit() {
         const con = textInput.value;
         console.log(`textInput: ${con}`);
-        if(con === ((this.content+" "))) this.visible = false;
-        textInput.value = "";
+        if(con == this.content) this.visible = false;
         draw();
     }
 }
@@ -41,7 +55,7 @@ class WordFactory {
     getRandomWords(words, canvX, vel) {
         // const word = new Word(randomWords(), canvX*(Math.random()), 0, vel);
         // let l = setInterval(() => words.push(new Word(randomWords(), canvX*(Math.random()), 0, vel)), period);
-        words.push(new Word(randomWords(), canvX*(Math.random()), 20, vel));
+        words.push(new Word(randomWords(), canvX*(Math.random()*0.8+0.1), 20, vel));
         return words;
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import '../css/style.css';
 import { Word, WordFactory } from './Word.js'
-import { WORD_LOAD_PERIOD, INIT_VELOCITY, SEA_LEVEL } from "./Constants";
+import { WORD_LOAD_PERIOD, WORD_DROP_PERIOD, INIT_VELOCITY, SEA_LEVEL } from "./Constants";
 import randomWords from 'random-words';
 // import {RWG} from 'randomwordsgenerator';
 
@@ -9,7 +9,7 @@ let words = [];
 function setup() {
     createCanvas(800, 600);
     let l = setInterval(() => {
-        words = WordFactory.getInstance().getRandomWords(words, WORD_LOAD_PERIOD, 800, INIT_VELOCITY);
+        words = WordFactory.getInstance().getRandomWords(words, 800, INIT_VELOCITY);
         // return words;
         console.log(words);
     }, WORD_LOAD_PERIOD);
@@ -21,10 +21,10 @@ function draw() {
     // let randomWords = require('random-words');
     fill(180, 255, 255);
     noStroke();
-    rect(0, 0, 800, 450);
+    rect(0, 0, 800, SEA_LEVEL);
     fill(0, 200, 255);
     noStroke();
-    rect(0, 450, 800, 150);
+    rect(0, 450, 800, 600-SEA_LEVEL);
     // const word = randomWords();
     // console.log(word);
     // let rwg = new RWG();
@@ -32,10 +32,19 @@ function draw() {
     // while (!isEmpty(words)) 
     // words[words.length-1].draw();
     // words[0].draw();
-    // for(let word of words) word.draw();
-    // let l = setInterval(() => {
-    //     words[words.length-1].draw();
-    // }, WORD_LOAD_PERIOD);
+    for(let word of words) {
+        word.draw();
+        word.inOcean();
+        if(!word.isVisible()) {
+            const index = words.indexOf(word);
+            if (index >= 0) words.splice(index, 1);
+            console.log(word.content);
+        }
+    }
+    let l = setInterval(() => {
+        for(let word of words) word.drop();
+    }, WORD_DROP_PERIOD); 
+    setTimeout(() => { clearInterval(l);}, WORD_DROP_PERIOD);
 }
 
 // const textInput = document.getElementById('textInput');
@@ -52,8 +61,9 @@ function keyPressed() {
         // textInput.value = "";
         for(let word of words) {
             word.submit();
-            word.draw();
+            // word.draw();
         }
+        textInput.value = "";
     }
 }
 
